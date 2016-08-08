d45f65fa7f8065f2ad87bd6bfa49512b6b29901a
Dogkiller69 <ingenieroolvera2010@gmail.com>
2016-08-08 13:42:39 -0500

    COMMIT main core architecture. folders - Empty Class

---
+++ app/src/main/java/mx/edu/tes/dev/tes/security/EmptyClass.java
@@ -0,0 +1,7 @@
+package mx.edu.tes.dev.tes.security;
+
+/**
+ * Created by perro on 08/08/2016.
+ */
+public class EmptyClass {
+}
---
+++ app/src/main/java/mx/edu/tes/dev/tes/service/EmptyClass.java
@@ -0,0 +1,7 @@
+package mx.edu.tes.dev.tes.service;
+
+/**
+ * Created by perro on 08/08/2016.
+ */
+public class EmptyClass {
+}
